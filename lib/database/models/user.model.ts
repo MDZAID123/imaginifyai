d231517561c8d720ceed8907c5f1788676ab40d0
MDZAID123 <88980148+MDZAID123@users.noreply.github.com>
2024-06-01 23:35:04 +0530

    initial commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model, models } from "mongoose"
+
+//here we will create Model for the user 
+const UserSchema=new Schema({
+    clerkId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    username:{
+        type:String,
+        required:true,
+        unqiue:true,
+    },
+    photo:{
+        type:String,
+        required:true,
+    },
+    firstName:{
+        type:String,
+    },
+    lastName:{
+        type:String,
+    },
+    planId:{
+        type:Number,
+        default:1,
+    },
+    creditBalance:{
+        type:Number,
+        default:10,
+    },
+});
+
+
+const User=models?.User ||model("User",UserSchema);
+export default User;
---
+++ lib/database/mongoose.ts
@@ -0,0 +1,27 @@
+// here we will write code  for connection to our mongodb database
+import mongoose,{Mongoose} from 'mongoose';
+
+const MONGODB_URL=process.env.MONGODB_URL;
+
+interface MongooseConnection{
+    conn:Mongoose|null;
+    promise:Promise<Mongoose>|null;
+}
+
+let cached:MongooseConnection=(global as any).mongoose
+if(!cached){
+    cached=(global as any).mongoose={
+        conn:null,promise:null
+    }
+}
+
+export const connectToDatabase=async()=>{
+    if(cached.conn) return cached.conn;
+    if(!MONGODB_URL) throw new Error('Missing MONOGDB_URL');
+
+    cached.promise=cached.promise||mongoose.connect(MONGODB_URL,{dbName:'IMAGINIFIAI',bufferCommands:false})
+
+    cached.conn= await cached.promise;
+    return cached.conn;
+
+}
